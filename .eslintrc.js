581a05e0c31cfe9021460ee7c90dd7336f28727a
Soham Sandeep Pawar <sohampofficial@gmail.com>
2024-07-19 22:07:56 +0530

    minor changes

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  plugins: ['json'], // Add the 'json' plugin
+  extends: ['eslint:recommended', 'plugin:json/recommended'], // Extend with JSON plugin recommendations
+  // Other configurations (optional)
+};
